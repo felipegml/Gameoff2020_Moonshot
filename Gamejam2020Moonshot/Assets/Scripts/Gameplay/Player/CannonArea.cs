334558277e19e54bd1ee584714cabd41ec3e3647
Felipe Gomes <felipegml@gmail.com>
2020-11-09 22:31:05 -0300

    Add cannon control

---
+++ Gamejam2020Moonshot/Assets/Scripts/Gameplay/Player/CannonArea.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay.Player
+{
+    public class CannonArea : MonoBehaviour
+    {
+        #region VARIABLES
+
+        [Header("Setup")]
+        public SpriteRenderer sprite;
+
+        #endregion
+
+        #region UNITY EVENTS
+
+        void Start()
+        {
+            StartCoroutine(Setup());
+        }
+
+        #endregion
+
+        #region SETUP
+
+        private IEnumerator Setup()
+        {
+            RectTransform _rect = gameObject.GetComponent<RectTransform>();
+            yield return new WaitForEndOfFrame();
+            sprite.size = _rect.sizeDelta;
+        }
+
+        #endregion
+    }
+}
